e91b0ae0cd6707ad1d50657438b205a22cb62b71
rosefishh <porose404@gmail.com>
2026-08-07 22:38:03 +0530

    question 9

--- pattern/q9.java
+++ pattern/q9.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class q9 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NO. OF ROWS:");
+        int n = sc.nextInt();
+
+        for(int i=0;i<n; i++){
+            for(int j=0; j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i+1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1; i--){
+            for(int j=n; j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=(2*i-1); k>=1;k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
