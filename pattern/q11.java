492e6e5662a4a6a03268eb522fc83bc561305285
rosefishh <porose404@gmail.com>
2026-08-07 23:39:16 +0530

    question 11

--- pattern/q11.java
+++ pattern/q11.java
@@ -9,10 +9,10 @@ public class q11 {
         for(int i=0;i<n; i++){
             for(int j=0; j<=i;j++){
                 if((i+j)%2==0){
-                    System.out.print("0 ");
+                    System.out.print("1 ");
                 }
                 else{
-                    System.out.print("1 ");
+                    System.out.print("0 ");
                 }
                     
             }
