cef36099cd63982b0a2c47ad2bdc31c5d147e02c
rosefishh <porose404@gmail.com>
2026-08-07 23:37:49 +0530

    question 11

---
+++ pattern/q11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NO. OF ROWS:");
+        int n = sc.nextInt();
+
+        for(int i=0;i<n; i++){
+            for(int j=0; j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("0 ");
+                }
+                else{
+                    System.out.print("1 ");
+                }
+                    
+            }
+            System.out.println();
+        }
+    }
+}
