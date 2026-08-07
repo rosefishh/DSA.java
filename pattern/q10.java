932c15adc30deea41c77aa9c6b2f43157eb3a858
rosefishh <porose404@gmail.com>
2026-08-07 23:08:53 +0530

    question 11

--- pattern/q10.java
+++ pattern/q10.java
@@ -13,8 +13,8 @@ public class q10 {
             System.out.println();
         }
 
-        for(int i=1;i<=n; i++){
-            for(int j=n; j>=i;j--){
+        for(int i=n-1;i>=1; i--){
+            for(int j=1; j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
