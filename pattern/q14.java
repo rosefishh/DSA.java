b76b495f8c1e76b83ffc5afb0c7bb87d7ceea41c
rosefishh <porose404@gmail.com>
2026-08-08 18:23:48 +0530

    question 14

--- pattern/q14.java
+++ pattern/q14.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
-public class q3 {
+public class q14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
-
-        for(int i=0; i<n; i++){
-            for(int j=1; j<=i+1; j++){
-                System.out.print(j);
+        for(int i=0; i<=n; i++){
+            for(char c= 'A';c<'A'+i ; c++){
+                System.out.print(c);
             }
             System.out.println();
         }
