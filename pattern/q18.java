704702fb2d35584c8c2a319b5cdece65cab4c22b
rosefishh <porose404@gmail.com>
2026-08-08 20:24:59 +0530

    question 18

--- pattern/q18.java
+++ pattern/q18.java
@@ -1,13 +1,18 @@
+
 import java.util.*;
 
-public class q14 {
+public class q18{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
-        for(int i=0; i<=n; i++){
-            for(char c= 'A';c<'A'+i ; c++){
-                System.out.print(c);
+        for(int i=1; i<=n; i++){
+
+            char c= (char)('A'+n-i);
+
+            for(int j=1;j<=i;j++){
+                System.out.print(c+" ");
+                c++;
             }
             System.out.println();
         }
