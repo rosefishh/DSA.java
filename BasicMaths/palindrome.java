19ead9e428ddb1213324d947a82b4ce60058d442
rosefishh <porose404@gmail.com>
2026-08-09 12:37:41 +0530

    question 3

--- BasicMaths/palindrome.java
+++ BasicMaths/palindrome.java
@@ -1,5 +1,26 @@
 package BasicMaths;
+import java.util.Scanner;
 
 public class palindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NO.:");
+        int n = sc.nextInt();
+
+        int rev=0;
+        int N=n;
+        while(n!=0){
+            int temp = n%10;
+            rev = rev*10+temp;
+            n=n/10;
+        }
+        
+
+        if(rev == N){
+            System.out.println("PALINDROME");
+        }
+        else
+            System.out.println("NOT PALINDROME");
+        
+    }
 }
