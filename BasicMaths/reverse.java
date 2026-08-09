f9c581c2a5bd69453d7c2a9a7f1b1dff48f12e8c
rosefishh <porose404@gmail.com>
2026-08-09 12:27:50 +0530

    question 2

--- BasicMaths/reverse.java
+++ BasicMaths/reverse.java
@@ -1,7 +1,5 @@
 package BasicMaths;
 import java.util.Scanner;
-import java.util.Scanner;
-
 public class reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
