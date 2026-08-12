02fbb8d4b3f3bd3835e8529c24c36001fb8aa7f5
rosefishh <porose404@gmail.com>
2026-08-12 18:59:44 +0530

    question 2

---
+++ recursion/q2.java
@@ -0,0 +1,14 @@
+package recursion;
+import java.util.*;
+public class q2 {
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+    static void ntimes(){
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        ntimes();
+    }
+}
