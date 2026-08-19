808e3e10c42af017aadce0559544202fc2868d82
rosefishh <porose404@gmail.com>
2026-08-19 23:36:34 +0530

    bubble sorting in java

---
+++ sorting/bubblesort.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class bubblesort {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        for(int i=0; i<arr.length;i++){
+        arr[i]= sc.nextInt();
+        }
+
+        for(int i= arr.length-1; i >= 1; i--){
+            for(int j=0; j <= i-1; j++){ 
+                if(arr[j] > arr[j+1]){ 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp;
+                }
+
+            }
+
+        }
+
+        System.out.print("Sorted Array:");
+        for (int i :arr) {
+            System.out.print(i+" ");
+        }
+        sc.close();
+    }
+}
